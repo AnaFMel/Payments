9a4a9b92ac5a6dc21ac33649b0ffe534459be9cc
AnaFMel <anaclara.freire.melo@gmail.com>
2026-06-25 20:04:39 -0300

    Adicionando logs para facilitar acompanhamento

--- PaymentsWorker/Consumers/OrderPlacedConsumer.cs
+++ PaymentsWorker/Consumers/OrderPlacedConsumer.cs
@@ -6,15 +6,23 @@ namespace PaymentsWorker.Consumers
 {
     public class OrderPlacedConsumer : IConsumer<OrderPlacedEvent>
     {
+        private readonly ILogger<OrderPlacedEvent> _logger;
+
+        public OrderPlacedConsumer(ILogger<OrderPlacedEvent> logger) => _logger = logger;
+
         public async Task Consume(ConsumeContext<OrderPlacedEvent> context)
         {
             var message = context.Message;
 
+            _logger.LogInformation($"User ID: {message.UserId}.\nGame ID:{message.GameId}.\nProcessando o pagamento...");
+
             var paymentStatus = PaymentService.SimulatePayment();
+
+            _logger.LogInformation($"Pagamento processado com sucesso! Resultado: {paymentStatus}");
 
             var paymentResult = new PaymentProcessedEvent
             {
-                TransactionId = new Guid(),
+                TransactionId = Guid.NewGuid(),
                 UserId = message.UserId,
                 UserEmail = message.UserEmail,
                 GameId = message.GameId,
