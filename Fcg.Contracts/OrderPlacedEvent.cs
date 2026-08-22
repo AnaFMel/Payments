8948cdeb51f3eb9e8122793c8d9b5429f261027c
splinterxsr <andre.pedroso34@gmail.com>
2026-08-22 11:29:00 -0300

    Alterado o tipo do campo gameId para string.

--- Fcg.Contracts/OrderPlacedEvent.cs
+++ Fcg.Contracts/OrderPlacedEvent.cs
@@ -4,11 +4,11 @@
     {
         public Guid Id { get; set; }
         public int UserId { get; set; }
-        public string UserEmail { get; set; } = null!;
-        public int GameId { get; set; }
+        public string UserEmail { get; set; } = string.Empty;
+        public string GameId { get; set; } = string.Empty;
         public decimal Price { get; set; }
 
-        public OrderPlacedEvent(int userId, string userEmail, int gameId, decimal price)
+        public OrderPlacedEvent(int userId, string userEmail, string gameId, decimal price)
         {
             Id = Guid.NewGuid();
             UserId = userId;
