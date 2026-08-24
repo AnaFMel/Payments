66730db06cd76b59c41598655b051fbdb4c4cbac
AnaFMel <anaclara.freire.melo@gmail.com>
2026-08-24 11:53:37 -0300

    Refatorando a mensageria para LocalStack

--- PaymentsWorker/Program.cs
+++ PaymentsWorker/Program.cs
@@ -1,31 +1,36 @@
+using Amazon.SimpleNotificationService;
+using Amazon.SQS;
 using MassTransit;
 using PaymentsWorker.Consumers;
+
 var builder = Host.CreateApplicationBuilder(args);
 
-#region MassTransit (RabbitMQ)
-
+#region MassTransit (AWS SQS / LocalStack)
 builder.Services.AddMassTransit(x =>
 {
     x.AddConsumer<OrderPlacedConsumer>();
 
-    x.UsingRabbitMq((context, cfg) =>
+    x.UsingAmazonSqs((context, cfg) =>
     {
-        var host = Environment.GetEnvironmentVariable("RABBITMQ_HOST") ?? "localhost";
-        var user = Environment.GetEnvironmentVariable("RABBITMQ_DEFAULT_USER") ?? "guest";
-        var password = Environment.GetEnvironmentVariable("RABBITMQ_DEFAULT_PASS") ?? "guest";
-
-        cfg.Host(host, "/", h =>
+        cfg.Host("us-east-1", h =>
         {
-            h.Username(user);
-            h.Password(password);
+            h.AccessKey("test");
+            h.SecretKey("test");
+
+            var awsEndpoint = Environment.GetEnvironmentVariable("AWS_ENDPOINT") ?? "http://localhost:4566";
+
+            h.Config(new AmazonSQSConfig { ServiceURL = awsEndpoint });
+            h.Config(new AmazonSimpleNotificationServiceConfig { ServiceURL = awsEndpoint });
         });
 
         var ordersPlacedQueue = Environment.GetEnvironmentVariable("ORDER_PLACED_QUEUE_NAME") ?? "orders-placed-queue";
 
-        cfg.ReceiveEndpoint(ordersPlacedQueue, e => e.ConfigureConsumer<OrderPlacedConsumer>(context));
+        cfg.ReceiveEndpoint(ordersPlacedQueue, e =>
+        {
+            e.ConfigureConsumer<OrderPlacedConsumer>(context);
+        });
     });
 });
-
 #endregion
 
 var host = builder.Build();
