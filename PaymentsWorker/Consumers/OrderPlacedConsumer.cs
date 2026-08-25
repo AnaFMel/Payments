f8e08a1fb6b08ef06136181a18babe3730beb70e
AnaFMel <anaclara.freire.melo@gmail.com>
2026-08-25 15:01:44 -0300

    Alterando publicação do evento

--- PaymentsWorker/Consumers/OrderPlacedConsumer.cs
+++ PaymentsWorker/Consumers/OrderPlacedConsumer.cs
@@ -7,8 +7,13 @@ namespace PaymentsWorker.Consumers
     public class OrderPlacedConsumer : IConsumer<OrderPlacedEvent>
     {
         private readonly ILogger<OrderPlacedEvent> _logger;
+        private readonly string _queueName;
 
-        public OrderPlacedConsumer(ILogger<OrderPlacedEvent> logger) => _logger = logger;
+        public OrderPlacedConsumer(ILogger<OrderPlacedEvent> logger, IConfiguration configuration)
+        {
+            _logger = logger;
+            _queueName = configuration["PAYMENTS_QUEUE_NAME"] ?? "payments-2-queue";
+        }
 
         public async Task Consume(ConsumeContext<OrderPlacedEvent> context)
         {
@@ -30,7 +35,10 @@ namespace PaymentsWorker.Consumers
                 Status = paymentStatus
             };
 
-            await context.Publish(paymentResult);
+            var sendEndpoint = await context.GetSendEndpoint(new Uri($"queue:{_queueName}"));
+            await sendEndpoint.Send(paymentResult);
+
+            _logger.LogInformation($"Evento 'PaymentProcessedEvent' enviado com sucesso para a fila {_queueName}!");
         }
     }
 }
