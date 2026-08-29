23a243da693a16828665969f0ac3f3b8db7d899e
AnaFMel <anaclara.freire.melo@gmail.com>
2026-08-28 22:46:00 -0300

    Enviando a mensagem do pagamento para duas filas diferentes

--- PaymentsWorker/Consumers/OrderPlacedConsumer.cs
+++ PaymentsWorker/Consumers/OrderPlacedConsumer.cs
@@ -7,12 +7,14 @@ namespace PaymentsWorker.Consumers
     public class OrderPlacedConsumer : IConsumer<OrderPlacedEvent>
     {
         private readonly ILogger<OrderPlacedEvent> _logger;
-        private readonly string _queueName;
+        private readonly string _queueCatalogName;
+        private readonly string _queueNotificationsName;
 
         public OrderPlacedConsumer(ILogger<OrderPlacedEvent> logger, IConfiguration configuration)
         {
             _logger = logger;
-            _queueName = configuration["PAYMENTS_QUEUE_NAME"] ?? "payments-2-queue";
+            _queueCatalogName = configuration["PAYMENTS_CATALOG_QUEUE_NAME"] ?? "payments-1-queue";
+            _queueNotificationsName = configuration["PAYMENTS_NOTIFICATION_QUEUE_NAME"] ?? "payments-2-queue";
         }
 
         public async Task Consume(ConsumeContext<OrderPlacedEvent> context)
@@ -35,10 +37,16 @@ namespace PaymentsWorker.Consumers
                 Status = paymentStatus
             };
 
-            var sendEndpoint = await context.GetSendEndpoint(new Uri($"queue:{_queueName}"));
-            await sendEndpoint.Send(paymentResult);
+            var sendEndpointCatalog = await context.GetSendEndpoint(new Uri($"queue:{_queueCatalogName}"));
+            await sendEndpointCatalog.Send(paymentResult);
 
-            _logger.LogInformation($"Evento 'PaymentProcessedEvent' enviado com sucesso para a fila {_queueName}!");
+            _logger.LogInformation($"Evento 'PaymentProcessedEvent' enviado com sucesso para a fila {_queueCatalogName}!");
+
+
+            var sendEndpointNotification = await context.GetSendEndpoint(new Uri($"queue:{_queueNotificationsName}"));
+            await sendEndpointNotification.Send(paymentResult);
+
+            _logger.LogInformation($"Evento 'PaymentProcessedEvent' enviado com sucesso para a fila {_queueNotificationsName}!");
         }
     }
 }
